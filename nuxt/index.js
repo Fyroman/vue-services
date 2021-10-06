740c0d0743664a8c95dc8f1595c498fac2b0d6d6
Fyroman <djamic.dominik@gmail.com>
2021-10-06 08:24:09 +0200

    fix: nuxt/index.js

--- nuxt/index.js
+++ nuxt/index.js
@@ -1,11 +1,2 @@
-import { resolve } from 'path'
-
-const NuxtModule = function (moduleOptions) {
-  this.addPlugin({
-    src: resolve(__dirname, './plugin.ejs'),
-    fileName: 'vueService.js',
-    options: this.options.vueServices,
-  })
-}
-
-export default NuxtModule
+import module from '../dist/nuxt.bundle.js'
+export default module
